ae500ebed29c1bfec32976b82444e46b53ea971a
Sam Black <sam.black@lubbockisd.org>
2023-02-21 08:56:03 -0600

    Notes Part 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+
+import static java.lang.System.out;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // Arrays
+        // an array of ints named dave numbered 0..22
+        int[] dave = new int[23];
+
+        out.println(dave[10]);      // direct access
+        out.println(dave);      // memory where the array starts
+
+        for (int index = 0; index < dave.length; index+=2 )
+            dave[index] = index * 12;
+
+        for (int index = 1; index < dave.length; index+=2 )
+            dave[index] = index * 4;
+
+        out.println(dave[11]);
+
+        out.println(Arrays.toString(dave));     //pretty prints the array
+
     }
 }
